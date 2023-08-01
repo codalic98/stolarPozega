15af13706c1758b79ee632f119efa0ca1afaa643
codalic98 <dacalukic586@gmail.com>
2023-08-01 17:26:17 +0200

    add tree imgs

--- StolarPozega/src/Pages/Home/Pocetna.tsx
+++ StolarPozega/src/Pages/Home/Pocetna.tsx
@@ -1,11 +1,13 @@
 import CorouselPocetna from "../../components/navbar/CorouselPocetna/CorouselPocetna";
 import Onama from "../../components/navbar/Onama/Onama";
+import TreeColl from "../../components/navbar/TreeColl/TreeColl";
 // import Usluge from "../../components/navbar/Usluge/Usluge";
 import "./pocetna.css";
 const Pocetna = () => {
   return (
     <>
       <CorouselPocetna />
+      <TreeColl />
       {/* <Usluge /> */}
       <Onama />
     </>
---
+++ StolarPozega/src/components/navbar/TreeColl/TreeColl.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const TreeColl = () => {
+  return (
+    <Container>
+      <br />
+      <br />
+      <Row>
+        <Col>
+          <h1>Ovde ide neki text</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia,
+            ex. Veniam natus quam, error tempora sed sint suscipit qui illum
+            cumque, illo officiis eum consequuntur voluptatem fugit atque nihil
+            excepturi!
+          </p>
+          <button>See more items</button>
+        </Col>
+        <Col>
+          <img
+            src="https://cb2.scene7.com/is/image/CB2/020323_super_furniture_2?wid=1000&qlt=50"
+            alt=""
+            style={{ height: "300px" }}
+          />
+        </Col>
+        <Col>
+          <img
+            src="https://cb2.scene7.com/is/image/CB2/020323_super_furniture_2?wid=1000&qlt=50"
+            alt=""
+            style={{ height: "300px" }}
+          />
+        </Col>
+        <Col>
+          <img
+            src="https://cb2.scene7.com/is/image/CB2/020323_super_furniture_2?wid=1000&qlt=50"
+            alt=""
+            style={{ height: "300px" }}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default TreeColl;
